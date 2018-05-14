36b1847ca4c53ec71c4489a506d11352f62daaed
Stephen Roberts <stephen.roberts@anu.edu.au>
2018-05-14 11:55:13 -0700

    Create install_anuga_colab.sh

---
+++ anuga_tools/install_anuga_colab.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+cd /content
+
+echo "(1) Install netcdf nose via pip"
+pip -q install netCDF4 nose  > /dev/null 2>&1 
+
+echo "(2) Install gdal via apt-get"
+apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1 
+
+echo "(3) Download anuga_core github repository"
+git clone --quiet https://github.com/GeoscienceAustralia/anuga_core.git  > /dev/null 2>&1 
+
+echo "(4) Install anuga"
+
+cd anuga_core
+python setup.py --quiet install  > /dev/null 2>&1 
+cd ../
+echo "(5) Ready to go"
