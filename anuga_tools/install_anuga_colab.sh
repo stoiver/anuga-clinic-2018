dfc6fdc18782379c213ef2350168f48da7a1bc74
Stephen Roberts <stephen.roberts@anu.edu.au>
2020-07-31 19:02:01 +1000

    Update install_anuga_colab.sh
    install netcdf4 via apt-get instead of pip

--- anuga_tools/install_anuga_colab.sh
+++ anuga_tools/install_anuga_colab.sh
@@ -2,19 +2,22 @@
 
 cd /content
 
-echo "(1) Install netcdf nose via pip"
-pip -q install netCDF4 nose  > /dev/null 2>&1 
+echo "(1) Install nose via pip"
+pip -q install nose  > /dev/null 2>&1 
 
 echo "(2) Install gdal via apt-get"
 apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1 
 
-echo "(3) Download anuga_core github repository"
+echo "(3) Install netcdf4 via apt-get"
+apt-get -q -y install python-netcdf4  > /dev/null 2>&1 
+
+echo "(4) Download anuga_core github repository"
 git clone --quiet https://github.com/GeoscienceAustralia/anuga_core.git  > /dev/null 2>&1 
 
-echo "(4) Install anuga"
+echo "(5) Install anuga"
 
 cd anuga_core
 python setup.py --quiet install  > /dev/null 2>&1 
 cd ../
 
-echo "(5) Ready to go"
+echo "(6) Ready to go"
