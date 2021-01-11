b3143ef53b9e6e69306bb9fd722495fc558a6f22
Stephen Roberts <stoiver@gmail.com>
2021-01-12 10:55:49 +1100

    Update install shell

--- anuga_tools/install_anuga_colab.sh
+++ anuga_tools/install_anuga_colab.sh
@@ -2,31 +2,32 @@
 
 cd /content
 
-echo "(1a) Install nose via pip"
+echo "Install Dependencies"
+echo "   Install nose via pip"
 pip -q install nose  > /dev/null 2>&1 
 
-echo "(1b) Install via gitpython pip"
+echo "   Install gitpython via pip"
 pip -q install gitpython  > /dev/null 2>&1 
 
-echo "(1c) Install via pyproj pip"
+echo "   Install pyproj via pip"
 pip -q install pyproj  > /dev/null 2>&1 
 
-echo "(2) Install gdal via apt-get"
+echo "   Install gdal via apt-get"
 apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1 
 
-echo "(3a) Install netcdf4 via apt-get"
+echo "   Install netcdf4 via apt-get"
 apt-get -q -y install python-netcdf4  > /dev/null 2>&1 
 
-echo "(3b) Install netcdf4 via pip"
+echo "   Install netcdf4 via pip"
 pip -q install netCDF4 > /dev/null 2>&1 
 
-echo "(4) Download anuga_core github repository"
+echo "(2) Download anuga_core github repository"
 git clone --quiet https://github.com/anuga-community/anuga_core.git  > /dev/null 2>&1 
 
-echo "(5) Install anuga"
+echo "(3) Install anuga"
 
 cd anuga_core
 python setup.py --quiet install  > /dev/null 2>&1 
 cd ../
 
-echo "(6) Ready to go"
+echo "(4) Ready to go"
