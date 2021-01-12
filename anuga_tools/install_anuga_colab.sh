b103c5f8f873632a8ea90e2f6c5e57ee4234b388
Stephen Roberts <stoiver@gmail.com>
2021-01-12 11:06:34 +1100

    install triangle

--- anuga_tools/install_anuga_colab.sh
+++ anuga_tools/install_anuga_colab.sh
@@ -19,7 +19,10 @@ echo "   Install netcdf4 via apt-get"
 apt-get -q -y install python-netcdf4  > /dev/null 2>&1 
 
 echo "   Install netcdf4 via pip"
-pip -q install netCDF4 > /dev/null 2>&1 
+pip -q install netCDF4 > /dev/null 2>&1
+
+echo "   Install triangle via pip"
+pip -q install triangle > /dev/null 2>&1 
 
 echo "(2) Download anuga_core github repository"
 git clone --quiet https://github.com/anuga-community/anuga_core.git  > /dev/null 2>&1 
