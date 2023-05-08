03a48a37074d7eb2b316629822c58d05ef09c457
Stephen Roberts <stoiver@gmail.com>
2023-05-08 20:37:17 +0930

    add script to create html files of executed notebooks

---
+++ notebooks/create_html.sh
@@ -0,0 +1,8 @@
+# If necessary first install appropriate extensions
+# conda install jupyter_contrib_nbextensions
+
+jupyter nbconvert notebook1.nbconvert.ipynb --to html
+jupyter nbconvert notebook2.nbconvert.ipynb --to html
+jupyter nbconvert notebook3.nbconvert.ipynb --to html
+jupyter nbconvert notebook4.nbconvert.ipynb --to html
+jupyter nbconvert notebook5.nbconvert.ipynb --to html
